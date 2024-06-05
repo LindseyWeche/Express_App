8604f81d2c038196a8c10317221a3cd6eda9d1ad
Lindsey Weche <lindseyhaishanw22@icloud.com>
2024-06-05 13:18:06 -0400

    commit3

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 // Import the module
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 
+app.set ('views', './views')
+
 //Set the view Engine Pug
-app.set('view engine', pug);
+app.set('view engine', 'pug');
+
+app.set ('views', path.join(_dirname, 'views'));
+
+app.get ('/', (req, res)=> {
+    res.render('index',{title: Welcome, message: 'Hello There'
+
+    })
+})
+
+
+// app.listen(port,()=>{
+//     console.log (`Server is running at port $(port)`);
+// });
 
 
